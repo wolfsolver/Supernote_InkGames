3beccf79b1d6c40d988049af7c8619ab60603ceb
Emmanuele (WolfSolver) <wolfsolver@prudenzano.org>
2026-03-01 20:41:02 +0100

    clearn

--- tmp/extract.ps1
+++
@@ -1,7 +0,0 @@
-$content = Get-Content "c:\Users\EmmanuelePrudenzano\Downloads\SuperNote\Supernote_InkGames\module\Nonogram\nonogram.js" -Raw
-$matches = [regex]::Matches($content, 'eval\("(.+?)"\)')
-foreach ($match in $matches) {
-    $evalStr = $match.Groups[1].Value
-    # JS string unescaping in PowerShell is tricky. Let's use a simpler way.
-    # We can use a temporary JS file and run it with node if available.
-}
